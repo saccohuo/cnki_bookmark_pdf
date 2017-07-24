ba12bac1652aa4f94b735f1ff7e5308131573df2
Husky <shuaike945@gmail.com>
2017-07-24 21:31:30 +0800

    change pdf from the recent file to the recent pdf

--- cnki_bookmark_pdf.py
+++ cnki_bookmark_pdf.py
@@ -6,12 +6,12 @@
 # 3. 利用爬虫获取书签页的所有文本（包括缩进信息）；Done
 # 4. 处理获取的书签文本，进行二次处理，参考“知网书签.bas”；Done
 # 5. 利用 pypdf2 和 pdfbookmarker 把处理之后的书签文本（含缩进）挂到 pdf 文件中。Done
+# 6. 现在如果只有第一个参数的话，是找到最近修改的文件，然后判断它是不是 pdf 的，后续可能需要改成找到最近的 pdf 文件，当然还要设定一定数量范围内，比如10个文件以内。Done
 #
 # TODO
 # 1. 可以借鉴油猴脚本直接获取 pdf 下载地址，并下载（curl or wget or aria2）。已下载到 “cnki_pdf_href_gen.js” 中。还是很有难度的。暂时用的 requests。
-# 2. 现在如果只有第一个参数的话，是找到最近修改的文件，然后判断它是不是 pdf 的，后续可能需要改成找到最近的 pdf 文件，当然还要设定一定数量范围内。
-# 3. 封装一下，就不需要安装 python 和这么多 package 的。暂时可以安装 anaconda（建议） 或 Python 原版（原版需要安装很多 package）。
-# 4. 解决参数中字符串形式的 Windows 路径最后的反斜杠被作为转义字符的问题。如“python cnki_bookmark_pdf.py "G:\IDMDownload\test_cnki\"”会导致最后的双引号被转义，出现识别错误。
+# 2. 封装一下，就不需要安装 python 和这么多 package 的。暂时可以安装 anaconda（建议） 或 Python 原版（原版需要安装很多 package）。
+# 3. 解决参数中字符串形式的 Windows 路径最后的反斜杠被作为转义字符的问题。如“python cnki_bookmark_pdf.py "G:\IDMDownload\test_cnki\"”会导致最后的双引号被转义，出现识别错误。
 
 
 import os
@@ -39,14 +39,15 @@ def getCopyText():
     wc.CloseClipboard()
     return copy_text
 
-def getRecentUpdateFile(base_dir):
+def getRecentPdf(base_dir, num):
     allfile = os.listdir(base_dir)
-    allfile.sort(key=lambda fn: os.path.getmtime(os.path.join(base_dir, fn)) if not os.path.isdir(os.path.join(base_dir, fn)) else 0)
-    recentfile = allfile[-1]
-    if os.path.splitext(recentfile)[1]=='.pdf':
-        return recentfile
-    else:
-        return None
+    allfile.sort(key=lambda fn: os.path.getmtime(os.path.join(base_dir, fn)) if not os.path.isdir(os.path.join(base_dir, fn)) else 0, reverse=True)
+    # recentfile = allfile[-1]
+    for ind,tpfile in enumerate(allfile):
+        if os.path.splitext(tpfile)[1]=='.pdf':
+            return tpfile
+        if ind==num-1:
+            return None
     return -1
 
 def getBookmarkUrl(url):
@@ -128,13 +129,14 @@ def cnki_get_bookmark(bookmarkfile, paperurl):
 
 def cnki_add_bookmark(pdffullpath=None, paperurl=None):
     defaultpath = 'G:/IDMDownload/'
+    recentnum = 10
     # defaultpath = os.getcwd()
     if pdffullpath == None or pdffullpath == '':
         pdfpath = defaultpath
-        pdffilename = getRecentUpdateFile(pdfpath)
+        pdffilename = getRecentPdf(pdfpath, recentnum)
     elif os.path.isdir(pdffullpath):
         pdfpath = pdffullpath
-        pdffilename = getRecentUpdateFile(pdfpath)
+        pdffilename = getRecentPdf(pdfpath, recentnum)
     else:
         curpdfpath = os.path.dirname(pdffullpath)
         curpdffilename = os.path.basename(pdffullpath)
@@ -148,7 +150,7 @@ def cnki_add_bookmark(pdffullpath=None, paperurl=None):
             else:
                 pdfpath = defaultpath
             if curpdffilename == '':
-                pdffilename = getRecentUpdateFile(pdfpath)
+                pdffilename = getRecentPdf(pdfpath, recentnum)
             else:
                 pdffilename = curpdffilename
 
